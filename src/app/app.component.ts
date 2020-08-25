5e252a7fdb812523ef69d82ceea06822f3608f58
Alejandro Arteaga <alejow18@gmail.com>
2020-08-25 14:23:33 -0500

    ejemplos de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+nombreRaro = 'AleJaNdRo aRtEaGA';
+nombre: string = 'Alejandro Arteaga';
+
+arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+PI: number = Math.PI;
+porcentaje: number = 0.328373;
+
+salario: number = 1234.456;
+
+fecha: Date = new Date();
+
+idioma: string = 'es';
+activar: boolean = true;
+
+videoUrl: string = 'https://www.youtube.com/embed/x2Y5ozcs870';
+
+valorPromesa = new Promise<string> ( (resolve) => {
+ setTimeout(() => {
+   resolve('llegaron los datos');
+
+ }, 4500);
+
+});
+
+
+heroe = {
+  nombre: 'Logan',
+  clave: 'Wolverine',
+  direccion: '5th avenue'
+       };
 }
